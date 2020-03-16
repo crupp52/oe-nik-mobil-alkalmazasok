45ce6f71bced2099d1fa0073de456030386fa6b9
Levente Zuck <zucklevente@gmail.com>
2020-03-16 13:06:09 +0100

    GY-04-0

---
+++ gy-04/app/src/main/java/com/crupp52/gy04/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.crupp52.gy04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.gy_04_0)
+    }
+}
